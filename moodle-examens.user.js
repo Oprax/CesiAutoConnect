d767eba131032e4ae7dda3d28e1d885dac02a7af
Hadrien Bècle <hadrien.becle@yahoo.fr>
2017-02-02 11:48:42 +0100

    Create moodle-examens.user.js

---
+++ moodle-examens.user.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name        MOODLE EXAMENS Auto-connect
+// @namespace   fr.hadrienbecle.exiademerde
+// @include     https://moodle-examens.cesi.fr/login/*
+// @version     1
+// @grant       none
+// ==/UserScript==
+
+window.location = "https://moodle-exia.cesi.fr/auth/mnet/jump.php?hostwwwroot=https%3A%2F%2Fmoodle-examens.cesi.fr&wantsurl=%2F&remoteurl=1";
