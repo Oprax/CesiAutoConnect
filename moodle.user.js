4928f80246c4ec878696a58f89b0109e93dd126a
Hadrien Bècle <hadrien.becle@yahoo.fr>
2017-02-02 11:46:02 +0100

    Create moodle.user.js

---
+++ moodle.user.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name        MOODLE Auto-connect
+// @namespace   fr.hadrienbecle.exiademerde
+// @include     https://moodle-exia.cesi.fr/login/*
+// @include     https://moodle-exia.cesi.fr/login/index.php?authCAS=CAS
+// @version     1
+// @grant       none
+// ==/UserScript==
+
+window.location = "https://moodle-exia.cesi.fr/login/index.php?authCAS=CAS";
